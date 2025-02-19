bfb5f3a672da58a6068bca765acce7fd5a569f9e
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2025-02-19 16:10:14 -0300

    Update pizzaria.c

--- pizzaria.c
+++ pizzaria.c
@@ -139,7 +139,7 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Parmê Pizzaria");
+    gtk_window_set_title(GTK_WINDOW(window), "Pizzaria");
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
